3773a4101903280e8c46f6b4af643390190550fe
wdy0808 <wdy0808@outlook.com>
2018-08-08 13:00:17 +0800

    添加状态类

--- threadpool/poolcondition.h
+++ threadpool/poolcondition.h
@@ -1,11 +1,27 @@
 #pragma once
+enum ThreadIncreaseWay{once_all, increase_linear, increase_index};
+class TaskQueue;
+class ThreadsWorker;
 class PoolCondition
 {
 public:
-	PoolCondition();
+	PoolCondition(ThreadIncreaseWay way, int start, int add);
 	~PoolCondition();
 
+	void activiteMoreThread();
+	void releaseOneThread();
+
+	int getWorkedThreadsNum();
+
+	void setIncreaseMode(ThreadIncreaseWay way, int num);
+	void setDecreaseMode(bool);
+
 private:
-	std::atomic<int> m_NeedKillThread;
+	std::map<std::thread::id, bool> m_ThreadCondition;
+	int m_StartNum, m_IncreaseNum, m_WorkedNum;
+	ThreadIncreaseWay m_Way;
+	std::atomic<bool> m_KillThread, m_Decrease;
+	TaskQueue* m_Task;
+	ThreadsWorker* m_Thread;
 };
 
